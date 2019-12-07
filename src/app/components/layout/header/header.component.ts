5cd830fe3320fa90d63d418cfa1d2f107e4c5cd7
Safronov Vladislav <safronov.zapas@gmail.com>
2019-12-07 20:11:27 +0300

    Add header layout with navigation links

---
+++ src/app/components/layout/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  private navBarOpened: boolean = false;
+  private linksList: Array<any> = [];
+
+  ngOnInit() {}
+
+  private toggleNavbar(): void {
+    this.navBarOpened = !this.navBarOpened;
+  }
+}
