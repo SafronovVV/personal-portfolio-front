68c848c88b5a1d2c2aaa9be894338d994501c1d6
Safronov Vladislav <safronov.zapas@gmail.com>
2019-12-07 21:22:51 +0300

    Add not found component Fix footer styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './components/layout/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+import { NotFoundComponent } from './components/layout/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
